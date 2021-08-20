250aa05ec337eda7e2717e8fbf97b531538ee33e
leobarna <82203547+leobarna@users.noreply.github.com>
2021-08-20 18:16:00 +0200

    Create SaluteRoboto.java

---
+++ src/main/java/SaluteRoboto.java
@@ -0,0 +1,8 @@
+public class SaluteRoboto {
+    public String answer(String phrase){
+         if (phrase.equals("¿Que tiempo hace en Barcelona?")){
+             return "Te vas a Barcelona y miras";
+         }
+         return "Hola";
+    }
+}
